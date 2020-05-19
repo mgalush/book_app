3d5bee49aa1572e679f1af9d79971f44fa472876
Melissa Galush <galushmn@gmail.com>
2020-05-18 20:24:45 -0700

    book objects rendering on page

--- server.js
+++ server.js
@@ -18,9 +18,49 @@ app.get('/hello', (req, res) => {
   res.render('pages/index');
 });
 
-app.get('/new', (req, res) => {
+app.get('/searches/new', (req, res) => {
   res.render('pages/searches/new');
 })
+
+app.get('searches/show', (req, res) => {
+  res.render('pages/searches/show');
+})
+
+app.post('/searches', searchBook);
+
+function searchBook(req, res) {
+  const url = 'https://www.googleapis.com/books/v1/volumes';
+  const {keyword, type} = req.body.search;
+  let q = '';
+
+  if (type === 'title') {
+    q+= `intitle:${keyword}`;
+  } else if (type === 'author') {
+    q+= `inauthor:${keyword}`;
+  } else {
+    res.send({message : 'type must be title or author'});
+  }
+ 
+  superagent.get(url)
+    .query({q})
+    .then((resultFromSuper) => {
+      const dataFromAPI = resultFromSuper.body.items;
+      const books = dataFromAPI.map(value => {
+        return new Book(value.volumeInfo);
+      })
+      res.render('pages/searches/show', { 'books': books });
+    })
+    .catch((error) => {
+      res.send(error);
+    })
+}
+
+function Book(obj) {
+  this.title = obj.title ? obj.title : 'Book Title';
+  this.author = obj.authors ? obj.authors[0] : 'Author';
+  this.image = obj.imageLinks.thumbnail ? obj.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
+  this.description = obj.description ? obj.description : 'Lorem epsum...';
+}
 
 
 app.listen(PORT, () => console.log(PORT));
