86f7067452e29bf1ffcca44569c3fd0fab9c2248
Melissa Galush <galushmn@gmail.com>
2020-05-19 20:01:56 -0700

    render books from database

--- server.js
+++ server.js
@@ -4,19 +4,24 @@ const express = require('express');
 const superagent = require('superagent');
 require('dotenv').config();
 
+// configs
 const PORT = process.env.port || 3000;
 const app = express();
+const pg = require('pg');
+
+// server config
+app.set('view engine', 'ejs');
 
 // form configs
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
-// server config
-app.set('view engine', 'ejs');
+// pg
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', console.error);
+client.connect();
 
-app.get('/', (req, res) => {
-  res.render('pages/index');
-});
+app.get('/', renderHomePage);
 
 app.get('/searches/new', (req, res) => {
   res.render('pages/searches/new');
@@ -48,9 +53,8 @@ function searchBook(req, res) {
   } else {
     res.send({message : 'type must be title or author'});
   }
- 
+  
   superagent.get(url)
-    .query({q})
     .then((resultFromSuper) => {
       const dataFromAPI = resultFromSuper.body.items;
       const books = dataFromAPI.map(value => {
@@ -59,7 +63,18 @@ function searchBook(req, res) {
       res.render('pages/searches/show', { 'books': books });
     })
     .catch((error) => {
-      res.render('/pages/error', { 'error' : error })
+      console.error(error);
+      res.render('pages/error', { 'error' : error })
+    })
+}
+
+function renderHomePage(req, res) {
+  client.query('SELECT * FROM books')
+    .then(result => {
+      res.render('pages/index', {
+        books : result.rows,
+        totalBookCount : result.rows.length,
+      });
     })
 }
 
@@ -75,6 +90,8 @@ function Book(obj) {
     }
   }
   this.image = obj.imageLinks.thumbnail ? obj.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
+  this.isbn = obj.industryIdentifiers[1] ? obj.industryIdentifiers[1].identifier : 'ISBN UNAVAILABLE';
+  // this.bookshelf = bookshelf;
 }
 
 
